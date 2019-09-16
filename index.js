5b252bff5748db973b0d3e1136cb952b4835cce9
McNally <Jack.McNally@sbdinc.com>
2019-09-16 11:01:09 -0400

    added the path package so it doesn't keep hitting blank pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 //Specify a port
 var port = process.env.port || 8080;
@@ -8,7 +9,9 @@ const bodyParser = require('body-parser');
 const Datastore = require('nedb');
 
 const database = new Datastore('database.db');
+const gameLog = new Datastore('gamelog.db');
 database.loadDatabase();
+gameLog.loadDatabase();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -23,5 +26,10 @@ app.use(express.json({ limit: '1mb'}));
 
 app.post('/user_create', (request, response) => {
   database.insert(request.body);
+  response.sendFile(path.join(__dirname + '/public/addplayer.html'));
+});
+
+app.post('/game_log', (request, response) => {
+  gameLog.insert(request.body);
   response.end();
 });
