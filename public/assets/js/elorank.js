e6d5a7ea80bdb301ae16c1ea17f4396de32598b4
McNally <Jack.McNally@sbdinc.com>
2019-09-17 17:48:20 -0400

    elo is implemented, but needs to be moved from client side to server side

--- public/assets/js/elorank.js
+++ public/assets/js/elorank.js
@@ -1,3 +1,5 @@
+
+
 getData();
 async function getData() {
   const response = await fetch('/loadplayers');
@@ -5,6 +7,46 @@ async function getData() {
   const response2 = await fetch('/loadgames');
   const games = await response2.json();
   var ranknum = 1;
+
+  //create object with K-Factor(without it defaults to 32)
+  var EloRank = require('elo-rank');
+  var elo = new EloRank();
+
+  var playerA = 1200;
+  var playerB = 1400;
+
+  //Gets expected score for first parameter
+  var expectedScoreA = elo.getExpected(playerA, playerB);
+  var expectedScoreB = elo.getExpected(playerB, playerA);
+
+  //update score, 1 if won 0 if lost
+  playerA = elo.updateRating(expectedScoreA, 1, playerA);
+  playerB = elo.updateRating(expectedScoreB, 0, playerB);
+
+  for (item of data) {
+    item.elo = 1000;
+  }
+
+  for (game of games) {
+    var winningNumber = -1;
+    var losingNumber = -1;
+
+    for (let i = 0; i < data.length; i++) {
+      if (`${game.winner}` == data[i].name) {
+        winningNumber = i;
+      }
+      if (`${game.losing}` == data[i].name) {
+        losingNumber = i;
+      }
+    }
+
+    var expectedScoreWinning = elo.getExpected(item[winningNumber].elo, item[losingNumber].elo);
+    var expectedScoreLosing = elo.getExpected(item[losingNumber].elo, item[winningNumber].elo);
+    item[winningNumber].elo = updateRating(expectedScoreWinning, 1, item[winningNumber]);
+    item[losingNumber].elo = updateRating(expectedScoreLosing, 0, item[losingNumber]);
+  }
+
+
 
   for (item of data) {
     var wins = 0;
@@ -33,7 +75,7 @@ async function getData() {
     const score = document.createElement('td');
     rank.textContent = ranknum.toString();
     name.textContent = `${item.name}`;
-    score.textContent = `${item.wins}`;
+    score.textContent = `${item.elo}`;
     row.append(rank, name, score);
     document.getElementById("tablebody").append(row);
     ranknum++;
