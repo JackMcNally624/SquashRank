ef4183a2c4476da231ebc5738808f6b85de4e947
McNally <Jack.McNally@sbdinc.com>
2019-09-17 15:17:49 -0400

    created elorank.js

---
+++ public/assets/js/elorank.js
@@ -0,0 +1,41 @@
+getData();
+async function getData() {
+  const response = await fetch('/loadplayers');
+  const data = await response.json();
+  const response2 = await fetch('/loadgames');
+  const games = await response2.json();
+  var ranknum = 1;
+
+  for (item of data) {
+    var wins = 0;
+    for (game of games) {
+      if (`${game.winner}` == `${item.name}`) {
+        wins++;
+      }
+    }
+    item.wins = wins;
+  }
+
+  for (let i = 1; i < data.length; i++) {
+    let j = i - 1;
+    let tmp = data[i];
+    while (j >= 0 && data[j].wins < tmp.wins) {
+      data[j + 1] = data[j];
+      j--;
+    }
+    data[j+1] = tmp;
+  }
+
+  for (item of data) {
+    const row = document.createElement('tr');
+    const rank = document.createElement('td');
+    const name = document.createElement('td');
+    const score = document.createElement('td');
+    rank.textContent = ranknum.toString();
+    name.textContent = `${item.name}`;
+    score.textContent = `${item.wins}`;
+    row.append(rank, name, score);
+    document.getElementById("tablebody").append(row);
+    ranknum++;
+  }
+}
