6f74cd40a30fd5f93dca18aa960516177b88ce00
McNally <Jack.McNally@sbdinc.com>
2019-09-18 11:34:00 -0400

    elo ranking actually worksgit add -A!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var EloRank = require('elo-rank');
+var elo = new EloRank();
 
 //Specify a port
 var port = process.env.port || 8080;
@@ -36,7 +38,31 @@ app.post('/game_log', (request, response) => {
 
 app.get('/loadplayers', (request, response) => {
   database.find({}, (err, data) => {
-    response.json(data);
+    for (item of data) {
+      item.elo = 1000;
+    }
+    gameLog.find({}, (err, games) => {
+      console.log(data);
+      for (game of games) {
+        var winningNumber = -1;
+        var losingNumber = -1;
+
+        for (let i = 0; i < data.length; i++) {
+          if (`${game.winner}` == data[i].name) {
+            winningNumber = i;
+          }
+          if (`${game.loser}` == data[i].name) {
+            losingNumber = i;
+          }
+        }
+        var expectedScoreWinning = elo.getExpected(data[winningNumber].elo, data[losingNumber].elo);
+        var expectedScoreLosing = elo.getExpected(data[losingNumber].elo, data[winningNumber].elo);
+        data[winningNumber].elo = elo.updateRating(expectedScoreWinning, 1, data[winningNumber].elo);
+        data[losingNumber].elo = elo.updateRating(expectedScoreLosing, 0, data[losingNumber].elo);
+      }
+      console.log(data);
+      response.json(data);
+    });
   });
 });
 
@@ -45,3 +71,45 @@ app.get('/loadgames', (request, response) => {
     response.json(data);
   });
 });
+
+/*
+
+//create object with K-Factor(without it defaults to 32)
+var EloRank = require('elo-rank');
+var elo = new EloRank();
+
+var playerA = 1200;
+var playerB = 1400;
+
+//Gets expected score for first parameter
+var expectedScoreA = elo.getExpected(playerA, playerB);
+var expectedScoreB = elo.getExpected(playerB, playerA);
+
+//update score, 1 if won 0 if lost
+playerA = elo.updateRating(expectedScoreA, 1, playerA);
+playerB = elo.updateRating(expectedScoreB, 0, playerB);
+
+for (item of data) {
+  item.elo = 1000;
+}
+
+for (game of games) {
+  var winningNumber = -1;
+  var losingNumber = -1;
+
+  for (let i = 0; i < data.length; i++) {
+    if (`${game.winner}` == data[i].name) {
+      winningNumber = i;
+    }
+    if (`${game.losing}` == data[i].name) {
+      losingNumber = i;
+    }
+  }
+
+  var expectedScoreWinning = elo.getExpected(item[winningNumber].elo, item[losingNumber].elo);
+  var expectedScoreLosing = elo.getExpected(item[losingNumber].elo, item[winningNumber].elo);
+  item[winningNumber].elo = updateRating(expectedScoreWinning, 1, item[winningNumber]);
+  item[losingNumber].elo = updateRating(expectedScoreLosing, 0, item[losingNumber]);
+}
+
+*/
