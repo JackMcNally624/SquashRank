5c4dd890523d17cc74bf0a79c334b4e072596381
McNally <Jack.McNally@sbdinc.com>
2019-12-04 06:22:50 -0500

    gamelog.html now lists games reverse chronologically

--- public/assets/js/gamelog.js
+++ public/assets/js/gamelog.js
@@ -5,6 +5,16 @@ async function getGameLog() {
   const response2 = await fetch('/loadgames');
   const games = await response2.json();
 
+  for (let i = 1; i < games.length; i++) {
+    let j = i - 1;
+    let tmp = games[i];
+    while (j >= 0 && games[j].time > tmp.time) {
+      games[j + 1] = games[j];
+      j--;
+    }
+    games[j+1] = tmp;
+  }
+
   for (item of games) {
     const row = document.createElement('tr');
     const winner = document.createElement('td');
